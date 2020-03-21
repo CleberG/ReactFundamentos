dac0963726021c2ab82fb78eaeeae29c3ebd31ce
Cleber Gomes Motta <cgmbnu@gmail.com>
2020-03-21 15:40:58 -0300

    Outras formas de criacao de compontes, criando componente criaComboBOx

--- fundamentos/src/App.js
+++ fundamentos/src/App.js
@@ -11,16 +11,28 @@ class App extends React.Component {
     nome: ''
   }
 
-  mudarNome = (event) => {
+  modificarNome = (event) => {
     this.setState({
       nome: event.target.value
     })
   }
+
+  criaComboBOx = () => {
+    const opcoes = ["Fulano" , "Cicrano"]
+  const comboBoxOpcoes = opcoes.map(opcao => <option>{opcao}</option>)
+  return(
+    <select>
+      {comboBoxOpcoes}
+    </select>
+  )
+  }
   render() {
+    const MeuComboBox = () => this.criaComboBOx()
     return (
       <>
-        <input type ="text" value = {this.state.value} onChange = {this.mudarNome}/>
+        <input type ="text" value = {this.state.value} onChange = {this.modificarNome}/>
         <h1>Hello {this.state.nome}</h1>
+        <MeuComboBox />
       </>
     )
   }
